900b3efd47b8b6574bda41f66d2097fee3c976f4
talex <talakuabraham@gmail.com>
2023-06-07 14:59:40 +0300

    Added command model to api project

---
+++ src/CommandApi/Models/Commands.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandApi.Models
+{
+    public class Commands
+    {
+        [Key]
+        [Required]
+        public int Id{get;set;}
+        [Required]
+        [MaxLength(250)]
+        public String HowTo{get;set;}
+        [Required]
+        public String Platform{get;set;}
+        [Required]
+        public String CommandLine{get;set;}
+    }
+}
